b461d810997a7e4f50136b3a25d218d90a80f47f
Alexander Maltsev <sasamaltsev981@gmail.com>
2024-09-15 19:25:53 +0600

    fix: upd ci

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDTO } from '@internal/dto/dto.user';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): CreateUserDTO {
+    return {
+      email: 'test',
+      password: 'test',
+    };
   }
 }
