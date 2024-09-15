99fac5bafd1418f340261f1c350ea6946d56af08
Alexander Maltsev <sasamaltsev981@gmail.com>
2024-09-15 20:07:01 +0600

    feat: add postgress connection code

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: 'postgres',
+    port: 5432,
+    username: 'postgres',
+    password: 'root',
+    database: 'nest',
+    autoLoadModels: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
